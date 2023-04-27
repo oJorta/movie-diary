c4aa9a1062fd9b16efdbc872d3577173d63124da
Joao Pedro <jpcsnoberto@gmail.com>
2023-04-26 22:13:34 -0300

    [done]: modal base script

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+const modal = document.querySelector('.modal');
+const addMovieBtn = document.querySelector('#add-movie');
+
+addMovieBtn.onclick = function(e){
+    modal.style.display = 'flex'
+}
+
+window.addEventListener('click', (e) =>{
+    if(e.target == modal){
+        modal.style.display = 'none'
+    }
+})
+
 let myDiary = [];
 
-function Movie(name, director, year, duration, watched){
+function Movie(name, director, year, duration, genre, watched){
     this.name = name;
     this.director = director;
     this.year = year;
     this.duration = duration;
+    this.genre = genre;
     this.watched = Boolean(watched) || false;
     this.movieIndex = myDiary.length;
 }
