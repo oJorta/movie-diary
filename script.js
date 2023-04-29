fd05946b3bb88853d399db3b7ff370adcd030f49
Joao Pedro <jpcsnoberto@gmail.com>
2023-04-29 17:18:02 -0300

    [done]: loadMovies function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector('.modal');
 const addMovieBtn = document.querySelector('#add-movie');
+const moviesGrid = document.querySelector('.movies-grid')
 
 addMovieBtn.onclick = function(e){
     modal.style.display = 'flex'
@@ -11,11 +12,28 @@ window.addEventListener('click', (e) =>{
     }
 })
 
-let myDiary = [];
+let myDiary = [
+    {
+        name: 'The Shining',
+        director: 'directed by Stanley Kubrick',
+        year: 1980,
+        duration: '2:26',
+        genre: 'Horror/Mistery',
+        watched: true
+    },
+    {
+        name: 'The Shining',
+        director: 'directed by Stanley Kubrick',
+        year: 1980,
+        duration: '2:26',
+        genre: 'Horror/Mistery',
+        watched: false
+    }
+];
 
 function Movie(name, director, year, duration, genre, watched){
     this.name = name;
-    this.director = director;
+    this.director = 'directed by ' + director;
     this.year = year;
     this.duration = duration;
     this.genre = genre;
@@ -25,10 +43,63 @@ function Movie(name, director, year, duration, genre, watched){
 
 function addMovieToLibrary(){
     /* let newMovie = new Movie(prompt(), prompt(), prompt(), prompt(), prompt()); */
-    let newMovie = new Movie('Creed III', 'Michael B. Jordan', 2023, '2:30', true);
+    let newMovie = new Movie('Creed III', 'Michael B. Jordan', 2023, '2:30', 'Action/Sports', true);
     myDiary.push(newMovie);
+}
+
+function loadMovies(){
+    myDiary.forEach( (movie) =>{
+        //console.log(movie);
+        let movieCard = document.createElement('div');
+        let movieInfo = document.createElement('div');
+        let moviePoster = document.createElement('div');
+        let watched = document.createElement('div');
+
+        let movieTitle = document.createElement('h2');
+        let movieGenre = document.createElement('p');
+        let movieDirector = document.createElement('p');
+        let movieDuration = document.createElement('p');
+
+        let watchedInput = document.createElement('input');
+        let watchedInputTag = document.createElement('p');
+
+        movieCard.classList.add('movie-card')
+        movieInfo.classList.add('movie-info')
+        moviePoster.classList.add('movie-poster')
+        watched.classList.add('watched-check')
+
+        movieTitle.innerText = movie.name + ` (${movie.year})`;
+        movieGenre.innerText = movie.genre;
+        movieDirector.innerText = movie.director;
+        movieDuration.innerText = movie.duration;
+
+        watchedInput.type = 'checkbox';
+        watchedInput.name = 'watched';
+        watchedInput.id = 'watched';
+        watchedInput.checked = movie.watched;
+        watchedInputTag.innerText = 'watched';
+
+
+
+        movieInfo.appendChild(movieTitle);
+        movieInfo.appendChild(movieGenre);
+        movieInfo.appendChild(movieDirector);
+        movieInfo.appendChild(movieDuration);
+
+        watched.appendChild(watchedInput);
+        watched.appendChild(watchedInputTag);
+
+        moviesGrid.appendChild(movieCard);
+        movieCard.appendChild(movieInfo);
+        movieCard.appendChild(moviePoster);
+        movieCard.appendChild(watched);
+
+        console.log(movieCard)
+
+    })
 }
 
 addMovieToLibrary();
 
-console.log(myDiary)
+loadMovies()
+//console.log(myDiary)
