fe80a37e2ee40b692aa8dde86c5f9e08515d9cee
Joao Pedro <jpcsnoberto@gmail.com>
2023-04-30 00:50:10 -0300

    [done]: add movie by form submit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const modal = document.querySelector('.modal');
+const form = document.querySelector('#add-movie-form');
 const addMovieBtn = document.querySelector('#add-movie');
-const moviesGrid = document.querySelector('.movies-grid')
+const moviesGrid = document.querySelector('.movies-grid');
+
+let myDiary = [
+    {
+        name: 'The Shining',
+        director: 'Stanley Kubrick',
+        year: 1980,
+        duration: '2:26',
+        genre: 'Horror/Mistery',
+        watched: true
+    },
+    {
+        name: 'Creed III',
+        director: 'Michael B. Jordan',
+        year: 2022,
+        duration: '2:30',
+        genre: 'Action/Sports',
+        watched: true
+    }
+];
+
+loadMovies();
 
 addMovieBtn.onclick = function(e){
     modal.style.display = 'flex'
@@ -10,47 +32,35 @@ window.addEventListener('click', (e) =>{
     if(e.target == modal){
         modal.style.display = 'none'
     }
-})
+});
 
-let myDiary = [
-    {
-        name: 'The Shining',
-        director: 'directed by Stanley Kubrick',
-        year: 1980,
-        duration: '2:26',
-        genre: 'Horror/Mistery',
-        watched: true
-    },
-    {
-        name: 'The Shining',
-        director: 'directed by Stanley Kubrick',
-        year: 1980,
-        duration: '2:26',
-        genre: 'Horror/Mistery',
-        watched: false
-    }
-];
+form.addEventListener('submit', (e) =>{
+
+    e.preventDefault();
+    let inputValues = getFormData();
+
+    addMovieToLibrary(inputValues[0], inputValues[1], inputValues[2], inputValues[3], inputValues[4], inputValues[5]);
+
+});
 
 function Movie(name, director, year, duration, genre, watched){
-    this.name = name;
-    this.director = 'directed by ' + director;
-    this.year = year;
+    this.name = name || '?';
+    this.director =  director;
+    this.year = year || '?';
     this.duration = duration;
-    this.genre = genre;
+    this.genre = genre || 'Not informed';
     this.watched = Boolean(watched) || false;
     this.movieIndex = myDiary.length;
 }
 
-function addMovieToLibrary(){
-    /* let newMovie = new Movie(prompt(), prompt(), prompt(), prompt(), prompt()); */
-    let newMovie = new Movie('Creed III', 'Michael B. Jordan', 2023, '2:30', 'Action/Sports', true);
+function addMovieToLibrary(name, director, year, duration, genre, watched){
+    let newMovie = new Movie(name, director, year, duration, genre, watched);
     myDiary.push(newMovie);
+    loadMovies();
 }
 
-function loadMovies(){
-    myDiary.forEach( (movie) =>{
-        //console.log(movie);
-        let movieCard = document.createElement('div');
+function showMovie(movie){
+    let movieCard = document.createElement('div');
         let movieInfo = document.createElement('div');
         let moviePoster = document.createElement('div');
         let watched = document.createElement('div');
@@ -59,7 +69,8 @@ function loadMovies(){
         let movieGenre = document.createElement('p');
         let movieDirector = document.createElement('p');
         let movieDuration = document.createElement('p');
-
+        let movieDurationSplit = movie.duration.split(':');
+        let formattedDuration =  movie.duration ? (Number(movieDurationSplit[0]) + 'h ' + movieDurationSplit[1] + 'm'):'Not informed'; 
         let watchedInput = document.createElement('input');
         let watchedInputTag = document.createElement('p');
 
@@ -70,16 +81,14 @@ function loadMovies(){
 
         movieTitle.innerText = movie.name + ` (${movie.year})`;
         movieGenre.innerText = movie.genre;
-        movieDirector.innerText = movie.director;
-        movieDuration.innerText = movie.duration;
+        movieDirector.innerText = 'directed by ' + movie.director;
+        movieDuration.innerText = formattedDuration;
 
         watchedInput.type = 'checkbox';
         watchedInput.name = 'watched';
         watchedInput.id = 'watched';
         watchedInput.checked = movie.watched;
         watchedInputTag.innerText = 'watched';
-
-
 
         movieInfo.appendChild(movieTitle);
         movieInfo.appendChild(movieGenre);
@@ -93,13 +102,26 @@ function loadMovies(){
         movieCard.appendChild(movieInfo);
         movieCard.appendChild(moviePoster);
         movieCard.appendChild(watched);
-
-        console.log(movieCard)
-
-    })
 }
 
-addMovieToLibrary();
+function loadMovies(){
+    moviesGrid.innerHTML = ''
+    myDiary.forEach( (movie) =>{
+        showMovie(movie);
+    });
+}
 
-loadMovies()
-//console.log(myDiary)
+function getFormData(){
+    let inputNodeList = form.querySelectorAll('input');
+    let inputValues = []
+    inputNodeList.forEach(input =>{
+        if(input.type === 'checkbox')
+            inputValues.push(input.checked)
+        else
+            inputValues.push(input.value)
+        
+        input.value = '';
+    });
+
+    return inputValues;
+}
